64a2b441e53ce5ccc5d30b1b4697e5dc2d1f60f1
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-07-10 03:39:53 +0200

    Display weather on cards

--- src/Components/Forecast/Forecast.tsx
+++ src/Components/Forecast/Forecast.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+
+// Components
+import Card from "./Card";
+
+// Styled Components
+import Titles from "../utils/Titles/Titles";
+const { Title2 } = Titles;
 
 // INTERFACE
 interface WeatherProp {
@@ -9,16 +17,39 @@ interface WeatherProp {
 
 const Forecast = ({ weatherData }: WeatherProp) => {
   console.log(weatherData);
+  const Forecast = styled.div`
+    width: 80%
+    margin: 1.4rem auto
+  `;
+
+  const ForecastTilte = styled(Title2)`
+    color: #000040;
+    text-align: center;
+    font-weight: 300;
+  `;
+
+  const GridView = styled.div`
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    grid-gap: 1rem;
+  `;
 
   return (
-    <div>
-      <h1>Forecast</h1>
-    </div>
+    <Fragment>
+      {Object.keys(weatherData).length > 0 && (
+        <Forecast>
+          <ForecastTilte>
+            {weatherData.city.name} - 5 Day / 3 Hours Forecast
+          </ForecastTilte>
+          <GridView>
+            {weatherData.list.map((item: object, index: number) => (
+              <Card key={index} weatherData={item} />
+            ))}
+          </GridView>
+        </Forecast>
+      )}
+    </Fragment>
   );
-};
-
-Forecast.propTypes = {
-  weatherData: PropTypes.object
 };
 
 const mapStateToProps = (state: any) => ({
